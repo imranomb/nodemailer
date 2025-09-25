2e7cace24363a58ae7c11ea439a37d96c1785d26
imranomb <imran2017omerbasic@hotmail.com>
2025-09-25 22:44:30 +0200

    mailsystem

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post('/send-email', async (req, res) => {
+  const { to, subject, text } = req.body;
+
+  try {
+    const transporter = nodemailer.createTransport({
+      service: 'gmail', // ili smtp konfiguracija
+      auth: {
+        user: 'Imran2018omerbasic@gmail.com',
+        pass: 'oiih divn vslz ityq'
+      }
+    });
+
+    await transporter.sendMail({
+      from: 'Imran2018omerbasic@gmail.com',
+      to,
+      subject,
+      text
+    });
+
+    res.status(200).send({ message: 'Email poslan!' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ message: 'Greška pri slanju emaila.' });
+  }
+});
+
+app.listen(5000, () => console.log('Server radi na portu 5000'));
